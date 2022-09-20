7e3a0743b5c695b2ff58ad44877a8421b8664b9f
K0tlyarski <djgame2.9.6@gmail.com>
2022-09-20 12:34:44 +0400

    Initial Commit

---
+++ prac2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  prac2
+//
+//  Created by Вадим Котлярский on 20.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
