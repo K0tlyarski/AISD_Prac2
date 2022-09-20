5432aeef9be2e20c5ba8adbb3907978bb99bbaf0
K0tlyarski <djgame2.9.6@gmail.com>
2022-09-20 12:56:47 +0400

    Практика 2

--- prac2/main.cpp
+++ prac2/main.cpp
@@ -6,6 +6,29 @@
 //
 
 #include <iostream>
+using namespace std;
+template <typename T>
+T MinInArr(T* arr,int size){
+    T tmp = arr[0];
+    for (int i =0; i<size; i++) {
+        if (tmp > arr[i]) {
+            tmp = arr[i];
+        }
+    }
+}
+
+template <typename T>
+T SearchElement (T* arr, int size, T index){
+    int tmp = 0;
+    cin>>tmp;
+    for (int i =0; i<size; i++) {
+        if (arr[i] == tmp) {
+            index = i;
+        }
+    }
+    cout<<index;
+  
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
